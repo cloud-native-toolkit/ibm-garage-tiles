712df7a3c9918505a7e10a11b062dd15c07f47c2
Sean Sundberg <seansund@us.ibm.com>
2020-07-21 07:25:10 -0500

    Updates Watson Assistant module version

--- terraform/stages/catalog/stage4-watson-assistant.tf
+++ terraform/stages/catalog/stage4-watson-assistant.tf
@@ -1,5 +1,5 @@
 module "dev_infrastructure_watson_assistant" {
-  source = "github.com/ibm-garage-cloud/terraform-ibm-watson-assistant.git?ref=v1.1.1"
+  source = "github.com/ibm-garage-cloud/terraform-ibm-watson-assistant.git?ref=v1.0.0"
 
   resource_group_name      = module.dev_cluster.resource_group_name
   resource_location        = module.dev_cluster.region
